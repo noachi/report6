b1fea5008ee26becdf0eebdc7d5ad575c6d005ec
Noa Teruya <e215715@ie.u-ryukyu.ac.jp>
2022-01-17 02:55:19 +0900

    humanクラスを追加

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e215715/Human.java
@@ -0,0 +1,36 @@
+package jp.ac.uryukyu.ie.e215715;
+import java.util.ArrayList;
+
+public class Human{
+    private String name;
+    private ArrayList<Card> hand = new ArrayList<Card>();
+
+    Human(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+    
+    public void addHand(Card card){
+        hand.add(card);
+        }
+
+    public int calculate(){
+        int num = 0;
+        for(var card : hand){
+            num += card.getIntegerNumber();
+        }
+        return num;
+    }
+
+    public Boolean isOver(){
+        if(calculate() > 21){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
